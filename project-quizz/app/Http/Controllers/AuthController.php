1227fdec54a3617a7b304c8c19ec4ccc737dd7b5
vutienminh1919 <vutineminh1919@gmail.com>
2022-01-03 20:13:49 +0700

    done login token  by using Sactum

--- project-quizz/app/Http/Controllers/AuthController.php
+++ project-quizz/app/Http/Controllers/AuthController.php
@@ -4,21 +4,62 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    public function register(Request $request)
+
+//    public function register(Request $request)
+//    {
+//        $request->validate([
+//            'name' => 'required',
+//            'email' => 'required',
+//            'password' => 'required|min:6|max:8',
+//        ]);
+//        $data = $request->only('name', 'email', 'password');
+//        $request['password'] = Hash::make($request->password);
+//        $user = User::query()->create($data);
+//        $user->save();
+//        return response()->json(['code' => 200, 'message' => 'Tạo tài khoản thành công ', 'data' => $data]);
+//    }
+
+    public function login(Request $request)
     {
-        $request->validate([
-            'name' => 'required',
-            'email' => 'required',
-            'password' => 'required|min:6|max:8',
-        ]);
-        $data = $request->only('name', 'email', 'password');
-        $request['password'] = Hash::make($request->password);
-        $user = User::query()->create($data);
-        $user->save();
-        return response()->json(['code' => 200, 'message' => 'Tạo tài khoản thành công ', 'data' => $data]);
+        try {
+            $request->validate([
+                'email' => 'email|required',
+                'password' => 'required'
+            ]);
+
+            $credentials = request(['email', 'password']);
+
+            if (!Auth::attempt($credentials)) {
+                return response()->json([
+                    'status_code' => 500,
+                    'message' => 'Unauthorized'
+                ]);
+            }
+
+            $user = User::where('email', $request->email)->first();
+
+            if (!Hash::check($request->password, $user->password, [])) {
+                throw new \Exception('Error in Login');
+            }
+
+            $tokenResult = $user->createToken('authToken')->plainTextToken;
+
+            return response()->json([
+                'status_code' => 200,
+                'access_token' => $tokenResult,
+                'token_type' => 'Bearer',
+            ]);
+        } catch (\Exception $error) {
+            return response()->json([
+                'status_code' => 500,
+                'message' => 'Error in Login',
+                'error' => $error,
+            ]);
+        }
     }
 }
--- project-quizz/routes/api.php
+++ project-quizz/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,4 +19,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::post('register',[AuthController::class,'register']);
+//Route::post('register',[AuthController::class,'register']);
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('/users',[UserController::class,'index']);
+});
+Route::post('/login', [AuthController::class,'login']);

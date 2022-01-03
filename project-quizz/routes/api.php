b41f5463083d3e02ca7e09c9975ef920dad965ac
vutienminh1919 <vutineminh1919@gmail.com>
2022-01-03 20:35:38 +0700

    done register by sactum, no front end

--- project-quizz/routes/api.php
+++ project-quizz/routes/api.php
@@ -19,8 +19,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-//Route::post('register',[AuthController::class,'register']);
+
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('/users',[UserController::class,'index']);
 });
 Route::post('/login', [AuthController::class,'login']);
+Route::post('/register', [AuthController::class, 'register']);

a4542f20a76a1a4a99fb82efc0ab80c6f256d8c4
unknown <hawk94210@gmail.com>
2022-01-04 09:15:27 +0700

    make relationship category to question

--- project-quizz/app/Models/Category.php
+++ project-quizz/app/Models/Category.php
@@ -12,4 +12,8 @@ class Category extends Model
     protected $fillable = [
       'name'
     ];
+
+    public function questions() {
+        return $this->hasMany(Question::class,'category_id','id');
+    }
 }

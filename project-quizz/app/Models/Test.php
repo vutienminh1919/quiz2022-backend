f79ec6d15a2d20b6ac385e88bd57a6f01500318f
unknown <hawk94210@gmail.com>
2022-01-04 09:38:18 +0700

    make relationship test to questions,users

--- project-quizz/app/Models/Test.php
+++ project-quizz/app/Models/Test.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Test extends Model
 {
     use HasFactory;
+
+    public function questions() {
+        return $this->belongsToMany(Question::class,'question_test','test_id','question_id');
+    }
+
+    public function users() {
+        return $this->belongsToMany(User::class,'test_user','test_id','user_id');
+    }
 }

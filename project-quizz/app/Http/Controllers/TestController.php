e64489e3603ea9945164111a6f72c31a46ee1e02
nguyenthixuananh <nguyenthixuananh09061999@gmail.com>
2022-01-06 16:51:17 +0700

    tao base repository

--- project-quizz/app/Http/Controllers/TestController.php
+++ project-quizz/app/Http/Controllers/TestController.php
@@ -5,13 +5,23 @@ namespace App\Http\Controllers;
 use App\Models\Question;
 use App\Models\Test;
 
+use App\Repositories\TestRepository;
 use App\Services\TestServiceImpl;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class TestController extends Controller
 {
+    protected $testRepository;
+    public function __construct(TestRepository $testRepository)
+    {
+     $this->testRepository= $testRepository;
+    }
 
+    public function index()
+    {
+        $tests = $this->testRepository->getAll();
+    }
 
 
 

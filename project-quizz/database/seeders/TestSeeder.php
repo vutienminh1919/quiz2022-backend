31616765c118962b8e3546f83ef71bce732e24a4
nguyenthixuananh <nguyenthixuananh09061999@gmail.com>
2022-01-06 17:36:12 +0700

    test api them sua xoa

--- project-quizz/database/seeders/TestSeeder.php
+++ project-quizz/database/seeders/TestSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class TestSeeder extends Seeder
 {
@@ -13,6 +15,18 @@ class TestSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('tests')->insert(
+            [
+                [
+                    'title' => "Test 1",
+                ],
+                [
+                    'title' => "Test 2",
+                ],
+                [
+                    'title' => "Test 3",
+                ],
+            ]
+        );
     }
 }

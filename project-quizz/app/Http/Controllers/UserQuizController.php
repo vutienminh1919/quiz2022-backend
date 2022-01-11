0ce0cb377b7a0ff520394b3962026c23ca2dff7f
unknown <hawk94210@gmail.com>
2022-01-12 05:39:13 +0700

    show result and doquiz in QuizResultController,UserQuizController

--- project-quizz/app/Http/Controllers/UserQuizController.php
+++ project-quizz/app/Http/Controllers/UserQuizController.php
@@ -2,9 +2,77 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\QuizQuestionService;
+use App\Services\QuizResultService;
+use App\Services\QuizService;
+use App\Services\UserQuizService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserQuizController extends Controller
 {
-    //
+    protected $quizQuestionService;
+    protected $quizService;
+    protected $userQuizService;
+    protected $quizResultService;
+
+    public function __construct(
+        QuizQuestionService $quizQuestionService,
+        QuizService $quizService,
+        UserQuizService $userQuizService,
+        QuizResultService $quizResultService
+    )
+    {
+        $this->quizResultService = $quizResultService;
+        $this->userQuizService = $userQuizService;
+        $this->quizService = $quizService;
+        $this->quizQuestionService = $quizQuestionService;
+    }
+
+    public function index($id)
+    {
+        $quizQuestion = $this->quizQuestionService->getQuestionsByQuizId($id);
+        $quiz = $this->quizService->findById($id);
+        return response()->json([$quizQuestion,$quiz]);
+    }
+
+    public function doQuiz(Request $request)
+    {
+        date_default_timezone_set('Asia/Ho_Chi_Minh');
+        $duration = $request->duration;
+        $now = time();
+        $ten_minutes = $now + ($duration * 60);
+        $start_time = date('Y-m-d H:i:s', $now);
+        $end_time = date('Y-m-d H:i:s', $ten_minutes);
+
+
+        $userQuizData = [
+            'user_id' => Auth::id(),
+            'quiz_id' => $request->quiz_id,
+            'start_time' => $start_time,
+            'end_time' => $end_time,
+            'finished' => 1
+        ];
+        $userQuiz = $this->userQuizService->create($userQuizData);
+
+        dd($userQuiz);
+        $answer_id = $request->answer_id;
+        $question_id[] = $request->question_id;
+        $correct = $request->correct;
+        $answered = $request->answered;
+
+        for ($i = 0; $i < count($question_id); $i++) {
+            $quizResultData = [
+                'user_id' => Auth::id(),
+                'quiz_id' => $request->quiz_id,
+                'question_id' => $question_id[$i],
+                'answer_id' => $answer_id[$i],
+                'correct' => $correct[$i],
+                'answered' => $answered[$i],
+                'user_quiz_id' => $userQuiz->id
+            ];
+            $this->quizResultService->create($quizResultData);
+        }
+        return response()->json(['quizId' => $userQuiz->id, 'userId' => Auth::id()]);
+    }
 }
--- project-quizz/app/Services/QuizResultService.php
+++ project-quizz/app/Services/QuizResultService.php
@@ -2,7 +2,45 @@
 
 namespace App\Services;
 
-class QuizResultService
-{
+use App\Repositories\QuizResultRepo;
 
+class QuizResultService implements CRUDinterfaceService
+{
+    protected $quizResultRepo;
+
+    public function __construct(QuizResultRepo $quizResultRepo)
+    {
+        $this->quizResultRepo = $quizResultRepo;
+    }
+
+    public function getAll()
+    {
+        return $this->quizResultRepo->getAll();
+    }
+
+    public function findById($id)
+    {
+        $quizResult = $this->quizResultRepo->findById($id);
+
+        return $quizResult;
+    }
+
+    public function create($request)
+    {
+        return $this->quizResultRepo->create($request);
+    }
+
+    public function update($id, $request)
+    {
+        $oldQuizResult = $this->quizResultRepo->findById($id);
+
+        return (!$oldQuizResult ? abort(404) : $this->quizResultRepo->update($request, $oldQuizResult));
+    }
+
+    public function destroy($id)
+    {
+        $quizResult = $this->quizResultRepo->findById($id);
+
+        return ($quizResult ? $this->quizResultRepo->destroy($quizResult) : abort(404));
+    }
 }
